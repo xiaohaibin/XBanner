22e4b77d596207f188a5c10e37e7a40c5410d805
Mr.xiao <xhb_199409@163.com>
2018-12-26 00:04:29 +0800

    优化demo

--- sample/src/main/java/com/stx/xhb/demo/CustomViewsActivity.java
+++ sample/src/main/java/com/stx/xhb/demo/CustomViewsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.stx.xhb.demo.entity.CustomViewsInfo;
 import com.stx.xhb.xbanner.XBanner;
@@ -31,13 +32,19 @@ public class CustomViewsActivity extends AppCompatActivity {
         data.add(new CustomViewsInfo("#8EE5EE"));
         data.add(new CustomViewsInfo("#00FA9A"));
         data.add(new CustomViewsInfo("#CD8162"));
-        banner.setBannerData(R.layout.layout_custom_view,data);
+        banner.setBannerData(R.layout.layout_custom_view, data);
         banner.loadImage(new XBanner.XBannerAdapter() {
             @Override
             public void loadBanner(XBanner banner, Object model, View view, int position) {
                 TextView tvContent = (TextView) view.findViewById(R.id.tv);
                 tvContent.setText(String.valueOf(position + 1));
                 view.setBackgroundColor(Color.parseColor(((CustomViewsInfo) model).getXBannerUrl()));
+            }
+        });
+        banner.setOnItemClickListener(new XBanner.OnItemClickListener() {
+            @Override
+            public void onItemClick(XBanner banner, Object model, View view, int position) {
+                Toast.makeText(CustomViewsActivity.this, "点击了" + position, Toast.LENGTH_SHORT).show();
             }
         });
     }
