d9aa7789afc98cc2d97cfdbfbedb71b08ad91c57
“xiaohaibin <xiao.haibin05485@westone.com.cn>
2018-12-27 16:46:57 +0800

    代码优化

--- sample/src/main/java/com/stx/xhb/demo/ClipChildrenModeActivity.java
+++ sample/src/main/java/com/stx/xhb/demo/ClipChildrenModeActivity.java
@@ -117,7 +117,8 @@ public class ClipChildrenModeActivity extends AppCompatActivity {
 
                         //刷新数据之后，需要重新设置是否支持自动轮播
                         mBanner.setAutoPlayAble(data.size() > 1);
-                        mBanner.setBannerData(R.layout.layout_fresco_imageview, data);
+                        //老方法，不推荐使用
+                        mBanner.setData(R.layout.layout_fresco_imageview, data,null);
 
 
                         //刷新数据之后，需要重新设置是否支持自动轮播
