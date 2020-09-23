08ebcd6aede3436b4309818beffb139d6c5ddb97
jxnk25 <xhb_199409@163.com>
2020-09-23 11:07:06 +0800

    新增showIndicatorInCenter属性控制一屏多显模式指示器显示

--- xbanner/src/main/java/com/stx/xhb/androidx/XBanner.java
+++ xbanner/src/main/java/com/stx/xhb/androidx/XBanner.java
@@ -269,6 +269,10 @@ public class XBanner extends RelativeLayout implements XBannerViewPager.AutoPlay
      * banner点击位置
      */
     private int clickPosition = -1;
+    /**
+     * 一屏多显模式下指示器是否显示在中间图片位置上，默认开启
+     */
+    private boolean mShowIndicatorInCenter;
 
     private ImageView.ScaleType mScaleType = ImageView.ScaleType.FIT_XY;
 
@@ -360,6 +364,7 @@ public class XBanner extends RelativeLayout implements XBannerViewPager.AutoPlay
             mIsShowTips = typedArray.getBoolean(R.styleable.XBanner_isShowTips, false);
             mBannerBottomMargin = typedArray.getDimensionPixelSize(R.styleable.XBanner_bannerBottomMargin, mBannerBottomMargin);
             mViewPagerClipChildren = typedArray.getBoolean(R.styleable.XBanner_viewPagerClipChildren, false);
+            mShowIndicatorInCenter = typedArray.getBoolean(R.styleable.XBanner_showIndicatorInCenter, true);
             int scaleTypeIndex = typedArray.getInt(R.styleable.XBanner_android_scaleType, -1);
             if (scaleTypeIndex >= 0 && scaleTypeIndex < sScaleTypeArray.length) {
                 mScaleType = sScaleTypeArray[scaleTypeIndex];
@@ -387,7 +392,7 @@ public class XBanner extends RelativeLayout implements XBannerViewPager.AutoPlay
         /*设定指示器容器布局及位置*/
         mPointContainerLp = new LayoutParams(RMP, RWC);
         mPointContainerLp.addRule(mPointContainerPosition);
-        if (mIsClipChildrenMode) {
+        if (mIsClipChildrenMode && mShowIndicatorInCenter) {
             mPointContainerLp.setMargins(mClipChildrenLeftRightMargin, 0, mClipChildrenLeftRightMargin, mClipChildrenTopBottomMargin);
         }
         addView(pointContainerRl, mPointContainerLp);
